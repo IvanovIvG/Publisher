96c1684435982d4844c19a7534394f2b1d47f3d7
IvanovIvG <pathfinder1123@mail.ru>
2025-01-29 18:06:19 +0300

    add lombok

--- src/main/java/ru/ivanov/Publisher/models/Article.java
+++ src/main/java/ru/ivanov/Publisher/models/Article.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.hibernate.annotations.JdbcType;
 import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 import ru.ivanov.Publisher.models.stagesEnums.FourStateStage;
@@ -15,6 +16,8 @@ import ru.ivanov.Publisher.models.stagesEnums.TwoStateStage;
  **/
 @Entity
 @Table(name = "article")
+@NoArgsConstructor
+@Data
 public class Article {
     @Id
     @Column(name = "id")
@@ -77,120 +80,4 @@ public class Article {
     @Enumerated(EnumType.STRING)
     @JdbcType(PostgreSQLEnumJdbcType.class)
     private TwoStateStage dummy = TwoStateStage.Not_Ready;
-
-    public Article() {
-    }
-
-    public Article(Journal journal, String name, String topic) {
-        this.journal = journal;
-        this.name = name;
-        this.topic = topic;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public @NotEmpty(message = "Name must not be empty") @Size(min = 1, max = 100, message = "Name must be from 1 to 100 length") String getName() {
-        return name;
-    }
-
-    public void setName(@NotEmpty(message = "Name must not be empty") @Size(min = 1, max = 100, message = "Name must be from 1 to 100 length") String name) {
-        this.name = name;
-    }
-
-    public @NotEmpty(message = "Topic must not be empty") @Size(min = 1, max = 100, message = "Topic must be from 1 to 100 length") String getTopic() {
-        return topic;
-    }
-
-    public void setTopic(@NotEmpty(message = "Topic must not be empty") @Size(min = 1, max = 100, message = "Topic must be from 1 to 100 length") String topic) {
-        this.topic = topic;
-    }
-
-    @Min(value = 0, message = "Number of pages must be positive")
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(@Min(value = 0, message = "Number of pages must be positive") int pages) {
-        this.pages = pages;
-    }
-
-    @Min(value = 0, message = "Number of first page must be positive")
-    public int getFirstPage() {
-        return firstPage;
-    }
-
-    public void setFirstPage(@Min(value = 0, message = "Number of first page must be positive") int firstPage) {
-        this.firstPage = firstPage;
-    }
-
-    @Min(value = 0, message = "Number of last page must be positive")
-    public int getLastPage() {
-        return lastPage;
-    }
-
-    public void setLastPage(@Min(value = 0, message = "Number of last page must be positive") int lastPage) {
-        this.lastPage = lastPage;
-    }
-
-    public FourStateStage getChiefEditor() {
-        return chiefEditor;
-    }
-
-    public void setChiefEditor(FourStateStage chiefEditor) {
-        this.chiefEditor = chiefEditor;
-    }
-
-    public ThreeStateStage getScienceEditor() {
-        return scienceEditor;
-    }
-
-    public void setScienceEditor(ThreeStateStage scienceEditor) {
-        this.scienceEditor = scienceEditor;
-    }
-
-    public ThreeStateStage getAuthorCoordination() {
-        return authorCoordination;
-    }
-
-    public void setAuthorCoordination(ThreeStateStage authorCoordination) {
-        this.authorCoordination = authorCoordination;
-    }
-
-    public ThreeStateStage getCorrector() {
-        return corrector;
-    }
-
-    public void setCorrector(ThreeStateStage corrector) {
-        this.corrector = corrector;
-    }
-
-    public TwoStateStage getPageProofs() {
-        return pageProofs;
-    }
-
-    public void setPageProofs(TwoStateStage pageProofs) {
-        this.pageProofs = pageProofs;
-    }
-
-    public TwoStateStage getDummy() {
-        return dummy;
-    }
-
-    public void setDummy(TwoStateStage dummy) {
-        this.dummy = dummy;
-    }
-
-    public @NotEmpty Journal getJournal() {
-        return journal;
-    }
-
-    public void setJournal(@NotEmpty Journal journal) {
-        this.journal = journal;
-    }
 }
--- src/main/java/ru/ivanov/Publisher/models/Journal.java
+++ src/main/java/ru/ivanov/Publisher/models/Journal.java
@@ -3,6 +3,7 @@ package ru.ivanov.Publisher.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +13,15 @@ import java.util.List;
  **/
 @Entity
 @Table(name = "journals")
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Journal {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private int id;
 
     @OneToMany(mappedBy="journal", cascade = CascadeType.REMOVE)
@@ -24,25 +30,7 @@ public class Journal {
     @Column(name = "name")
     @NotEmpty(message = "Name must not be empty")
     @Size(min = 1, max = 100, message = "Name must be from 1 to 100 length")
+    @Getter
+    @Setter
     private String name;
-
-
-    public Journal() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public @NotEmpty(message = "Name must not be empty") @Size(min = 1, max = 100, message = "Name must be from 1 to 100 length") String getName() {
-        return name;
-    }
-
-    public void setName(@NotEmpty(message = "Name must not be empty") @Size(min = 1, max = 100, message = "Name must be from 1 to 100 length") String name) {
-        this.name = name;
-    }
 }
