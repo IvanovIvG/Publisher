631ae85172ae6a260777f49ebb4c439fa899d2a3
IvanovIvG <pathfinder1123@mail.ru>
2025-03-07 17:29:28 +0300

    add security server

--- BackEnd/src/main/java/ru/ivanov/backend/configs/SecurityConfig.java
+++ BackEnd/src/main/java/ru/ivanov/backend/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.ivanov.backend.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,25 +16,46 @@ import java.util.List;
  **/
 @Configuration
 public class SecurityConfig {
+    @Value("${keySetURI}")
+    private String keySetUri;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(c -> {
-            CorsConfigurationSource source = request -> {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(
-                        List.of("http://localhost:3000"));
-                config.setAllowedMethods(
-                        List.of("*"));
-                config.setAllowedHeaders(List.of("*"));
-                return config;
-            };
-            c.configurationSource(source);
-        });
+        http.oauth2ResourceServer(
+                c -> c.jwt(
+                        j -> j.jwkSetUri(keySetUri)
+                )
+        );
+
         http.csrf(
                 AbstractHttpConfigurer::disable
         );
-        http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
         return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.cors(c -> {
+//            CorsConfigurationSource source = request -> {
+//                CorsConfiguration config = new CorsConfiguration();
+//                config.setAllowedOrigins(
+//                        List.of("http://localhost:3000"));
+//                config.setAllowedMethods(
+//                        List.of("*"));
+//                config.setAllowedHeaders(List.of("*"));
+//                return config;
+//            };
+//            c.configurationSource(source);
+//        });
+//        http.csrf(
+//                AbstractHttpConfigurer::disable
+//        );
+//        http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+//        return http.build();
+//    }
 }
--- BackEnd/src/main/java/ru/ivanov/backend/stagesEnums/ThreeStateStage.java
+++ BackEnd/src/main/java/ru/ivanov/backend/stagesEnums/ThreeStateStage.java
@@ -4,5 +4,5 @@ package ru.ivanov.backend.stagesEnums;
  * @author Ivan Ivanov
  **/
 public enum ThreeStateStage {
-    Ready, In_Process, Not_Ready;
+    Ready, In_Process, Not_Ready
 }
