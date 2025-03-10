7dd9856c9abc4a871fd30bc9d10d9548e8b1f468
IvanovIvG <pathfinder1123@mail.ru>
2025-03-10 19:38:38 +0300

    it works!!!

--- SecurityServer/src/main/java/ru/ivanov/securityserver/configs/SecurityConfig.java
+++ SecurityServer/src/main/java/ru/ivanov/securityserver/configs/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +19,8 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -58,25 +61,22 @@ public class SecurityConfig {
                         new LoginUrlAuthenticationEntryPoint("/login"))
         );
 
-        http.oauth2ResourceServer(
-                c -> c.jwt(
-                        j -> j.jwkSetUri("http://localhost:9090/oauth2/jwks")
-                )
-        );
-
         return http.build();
     }
 
     @Bean
     @Order(2)
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-            throws Exception {
-        http.formLogin(Customizer.withDefaults());
-
-        http.authorizeHttpRequests(
-                c -> c.anyRequest().authenticated()
-        );
-
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) ->
+                        authorize
+                                .anyRequest().authenticated()
+                )
+                // ADD THIS LINE HERE
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults());
         return http.build();
     }
 
@@ -128,6 +128,11 @@ public class SecurityConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 
     @Bean
